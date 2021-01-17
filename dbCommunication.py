df99671471a3218edb0b5bf1de7260a198c65774
ZacetekVaj <44202366+ZacetekVaj@users.noreply.github.com>
2021-01-17 15:21:40 +0100

    Update dbCommunication.py

--- dbCommunication.py
+++ dbCommunication.py
@@ -294,7 +294,7 @@ class MovieDatabase(Connector):
 
         return movies_data
 
-    def get_movie_by_param(self, param):
+    def get_movie_by_param(self, parameters):
         """ todo Function that returns movie/director data based on search inputs saved in dict param
             todo should add: If key not in param, user did not select that
         :param param: dict('release_year': dict('from': , 'to': ),
@@ -306,6 +306,298 @@ class MovieDatabase(Connector):
                 if from/to == -1, do all
         :return: the same
         """
+        def get_all_movies():
+            '''Searches movies'''
+
+            # List to save the movie data in based on id, used for interjection
+            movies_data = {}
+            # Create cursor
+            self.create_cursor()
+            code = "SELECT * FROM movie"
+            param = (None)
+            self.cur.execute(code, param)
+
+            # Save all of the data for movies
+            for movie in self.cur:
+                idMovie = movie['idMovie']
+                title = movie['title']
+                isAdult = movie['isAdult']
+                releaseYear = movie['releaseYear']
+                runtimeMinutes = movie['runtimeMinutes']
+                rating = movie['rating']
+                numVotes = movie['numVotes']
+
+                img_url = get_google_image_link(title + " " + str(releaseYear))
+                additional_data = get_movie_details(id)
+
+                movies_dict = {'title': title,
+                               'isAdult': isAdult,
+                               'releaseYear': releaseYear,
+                               'runtimeMinutes': runtimeMinutes,
+                               'rating': rating,
+                               'numVotes': numVotes,
+                               'img_url': img_url,
+                               'description': additional_data['description']}
+                movies_data[idMovie] = movies_dict
+            return movies_data
+            
+        def get_movie_by_genre(genre):
+            '''Searches movies by genres'''
+
+            # List to save the movie data in based on id, used for interjection
+            movies_data = {}
+            # Create cursor
+            self.create_cursor()
+            code = "SELECT movie.* FROM movie JOIN genresbymovie ON genresbymovie.idMovie = movie.idMovie JOIN genres ON genresbymovie.idGenre = genre.idGenre WHERE genre.genreName = %s"
+            param = (genre,)
+            self.cur.execute(code, param)
+
+            # Save all of the data for movies
+            for movie in self.cur:
+                idMovie = movie['idMovie']
+                title = movie['title']
+                isAdult = movie['isAdult']
+                releaseYear = movie['releaseYear']
+                runtimeMinutes = movie['runtimeMinutes']
+                rating = movie['rating']
+                numVotes = movie['numVotes']
+
+                img_url = get_google_image_link(title + " " + str(releaseYear))
+                additional_data = get_movie_details(id)
+
+                movies_dict = {'title': title,
+                               'isAdult': isAdult,
+                               'releaseYear': releaseYear,
+                               'runtimeMinutes': runtimeMinutes,
+                               'rating': rating,
+                               'numVotes': numVotes,
+                               'img_url': img_url,
+                               'description': additional_data['description']}
+                movies_data[idMovie] = movies_dict
+
+            if movies_data == {}:
+                return all_movies
+            return movies_data
+        
+        def get_movie_by_releaseYear(fr=0,to=10000):
+            '''Searches movies by year'''
+            if to == -1:
+                to = 100000
+            # List to save the movie data in based on id, used for interjection
+            movies_data = {}
+            # Create cursor
+            self.create_cursor()
+            code = "SELECT * FROM movie WHERE releaseYear > %s AND releaseYear < %s"
+            param = (fr,to)
+            self.cur.execute(code, param)
+
+            # Save all of the data for movies
+            for movie in self.cur:
+                idMovie = movie['idMovie']
+                title = movie['title']
+                isAdult = movie['isAdult']
+                releaseYear = movie['releaseYear']
+                runtimeMinutes = movie['runtimeMinutes']
+                rating = movie['rating']
+                numVotes = movie['numVotes']
+
+                img_url = get_google_image_link(title + " " + str(releaseYear))
+                additional_data = get_movie_details(id)
+
+                movies_dict = {'title': title,
+                               'isAdult': isAdult,
+                               'releaseYear': releaseYear,
+                               'runtimeMinutes': runtimeMinutes,
+                               'rating': rating,
+                               'numVotes': numVotes,
+                               'img_url': img_url,
+                               'description': additional_data['description']}
+                movies_data[idMovie] = movies_dict
+
+            if movies_data == {}:
+                return all_movies
+            return movies_data
+        
+        def get_movie_by_duration(fr=0,to=10000):
+            '''Searches movies by duration'''
+            if to == -1:
+                to = 100000
+            # List to save the movie data in based on id, used for interjection
+            movies_data = {}
+            # Create cursor
+            self.create_cursor()
+            code = "SELECT * FROM movie WHERE runtimeMinutes > %s AND runtimeMinutes < %s"
+            param = (fr,to)
+            self.cur.execute(code, param)
+
+            # Save all of the data for movies
+            for movie in self.cur:
+                idMovie = movie['idMovie']
+                title = movie['title']
+                isAdult = movie['isAdult']
+                releaseYear = movie['releaseYear']
+                runtimeMinutes = movie['runtimeMinutes']
+                rating = movie['rating']
+                numVotes = movie['numVotes']
+
+                img_url = get_google_image_link(title + " " + str(releaseYear))
+                additional_data = get_movie_details(id)
+
+                movies_dict = {'title': title,
+                               'isAdult': isAdult,
+                               'releaseYear': releaseYear,
+                               'runtimeMinutes': runtimeMinutes,
+                               'rating': rating,
+                               'numVotes': numVotes,
+                               'img_url': img_url,
+                               'description': additional_data['description']}
+                movies_data[idMovie] = movies_dict
+            if movies_data == {}:
+                return all_movies
+            return movies_data
+        
+        def get_movie_by_director(name):
+            '''Searches movies by director'''
+
+            # List to save the movie data in based on id, used for interjection
+            movies_data = {}
+            # Create cursor
+            self.create_cursor()
+            code = "SELECT movie.* FROM movie JOIN team ON team.idMovie = movie.idMovie JOIN writersanddirectors ON team.idWritersAndDirectors = writersanddirectors.idWritersAndDirectors WHERE writersanddirectors.name = %s"
+            param = (name,)
+            self.cur.execute(code, param)
+
+            # Save all of the data for movies
+            for movie in self.cur:
+                idMovie = movie['idMovie']
+                title = movie['title']
+                isAdult = movie['isAdult']
+                releaseYear = movie['releaseYear']
+                runtimeMinutes = movie['runtimeMinutes']
+                rating = movie['rating']
+                numVotes = movie['numVotes']
+
+                img_url = get_google_image_link(title + " " + str(releaseYear))
+                additional_data = get_movie_details(id)
+
+                movies_dict = {'title': title,
+                               'isAdult': isAdult,
+                               'releaseYear': releaseYear,
+                               'runtimeMinutes': runtimeMinutes,
+                               'rating': rating,
+                               'numVotes': numVotes,
+                               'img_url': img_url,
+                               'description': additional_data['description']}
+                movies_data[idMovie] = movies_dict
+            if movies_data == {}:
+                return all_movies
+            return movies_data
+        
+        def get_movie_by_numVotes(fr=0,to=10000):
+            '''Searches movies by number of votes'''
+            
+            if to == -1:
+                to = 100000
+                
+            # List to save the movie data in based on id, used for interjection
+            movies_data = {}
+            # Create cursor
+            self.create_cursor()
+            code = "SELECT * FROM movie WHERE numVotes > %s AND numVotes < %s"
+            param = (fr,to)
+            self.cur.execute(code, param)
+
+            # Save all of the data for movies
+            for movie in self.cur:
+                idMovie = movie['idMovie']
+                title = movie['title']
+                isAdult = movie['isAdult']
+                releaseYear = movie['releaseYear']
+                runtimeMinutes = movie['runtimeMinutes']
+                rating = movie['rating']
+                numVotes = movie['numVotes']
+
+                img_url = get_google_image_link(title + " " + str(releaseYear))
+                additional_data = get_movie_details(id)
+
+                movies_dict = {'title': title,
+                               'isAdult': isAdult,
+                               'releaseYear': releaseYear,
+                               'runtimeMinutes': runtimeMinutes,
+                               'rating': rating,
+                               'numVotes': numVotes,
+                               'img_url': img_url,
+                               'description': additional_data['description']}
+                movies_data[idMovie] = movies_dict
+            if movies_data == {}:
+                return all_movies
+            return movies_data       
+
+        def get_movie_by_rating(fr=0,to=10000):
+            '''Searches movies by rating'''
+            
+            if to == -1:
+                to = 100000
+                
+            # List to save the movie data in based on id, used for interjection
+            movies_data = {}
+            # Create cursor
+            self.create_cursor()
+            code = "SELECT * FROM movie WHERE rating > %s AND rating < %s"
+            param = (fr,to)
+            self.cur.execute(code, param)
+
+            # Save all of the data for movies
+            for movie in self.cur:
+                idMovie = movie['idMovie']
+                title = movie['title']
+                isAdult = movie['isAdult']
+                releaseYear = movie['releaseYear']
+                runtimeMinutes = movie['runtimeMinutes']
+                rating = movie['rating']
+                numVotes = movie['numVotes']
+
+                img_url = get_google_image_link(title + " " + str(releaseYear))
+                additional_data = get_movie_details(id)
+
+                movies_dict = {'title': title,
+                               'isAdult': isAdult,
+                               'releaseYear': releaseYear,
+                               'runtimeMinutes': runtimeMinutes,
+                               'rating': rating,
+                               'numVotes': numVotes,
+                               'img_url': img_url,
+                               'description': additional_data['description']}
+                movies_data[idMovie] = movies_dict
+            if movies_data == {}:
+                return all_movies
+            return movies_data
+
+        def join_all():
+             '''dict('release_year': dict('from': , 'to': ),
+                            'genre': str(),
+                            'duration': dict('from': , 'to': ),
+                            'directed_by': str(),
+                            'number_of_votes': dict('from': , 'to': ),
+                            'rating': dict('from': , 'to': ))'''
+            data = []
+            d1 = get_movie_by_rating(parameters['rating']['from'],parameters['rating']['to'])
+            d2 = get_movie_by_numVotes(parameters['rating']['from'],parameters['rating']['to'])
+            d3 = get_movie_by_director(parameters['directed_by'])
+            d4 = get_movie_by_duration(parameters['duration']['from'],parameters['duration']['to'])
+            d5 = get_movie_by_genre(parameters['genre'])
+            d6 = get_movie_by_releaseYear(parameters['release_year']['from'],parameters['release_year']['to'])
+            
+            common_ids = d1.keys() & d2.keys() & d3.keys() & d4.keys() & d5.keys() & d6.keys()
+            for mov_id in all_movies:
+                if mov_id in common_ids:
+                    data.append(all_movies[mov_id])
+            return data
+
+        all_movies = get_all_movies()
+        
+        return join_all()
+
 
     def get_movie_by_param_randomized(self, param):
         """ todo Function that returns one movie based on param, randomized from all the movies that fit param measures
